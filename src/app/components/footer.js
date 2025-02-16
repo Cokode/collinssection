faeafd47f58c3e13590cacba2939d2d51dd96a4a
Collins C. A. <contactmrc4@gmail.com>
2025-02-16 18:43:24 +0100

    refactored

---
+++ src/app/components/footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import styles from "@/app/page.module.css";
+
+export default function Footer () {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="#"
+        target="_blank"
+        // rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        Welcome to my portfolio! This web page is under construction, soon you can explore some of my certifications, projects, and blogs.
+      </a>
+    </footer>
+  );
+};
