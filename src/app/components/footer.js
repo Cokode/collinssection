e5c6209b0e3066a224dfdf5d780c82eb0b1a49f8
Collins C. A. <contactmrc4@gmail.com>
2025-03-14 12:35:08 +0100

    Fixed: render issue

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -5,22 +5,15 @@ import styles from "@/app/page.module.css";
 export default function Footer () {
   return (
     <footer className={styles.footer}>
-      <a
-        // href="#"
-        // // target="_blank"
-        // // rel="noopener noreferrer"
-        
-      >
-        <Image
-          style={styles.footerIcon}
-          aria-hidden
-          src="/globe.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
-        Welcome to my portfolio! This web page is under construction, soon you can explore my certifications, projects, and blogs.
-      </a>
+      {/* <Image
+        className={styles.footerIcon}
+        aria-hidden
+        src="/globe.svg"
+        alt="Globe icon"
+        width={16}
+        height={16}
+      /> */}
+        <p className="footerWord">Welcome to my portfolio! This web page is under construction, soon you can explore my certifications, projects, and blogs.</p>
     </footer>
   );
 };
